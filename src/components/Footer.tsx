e8849ae9f11b5f56123361f8c80f069de2d15cc8
Selaelo1 <selaelolanga10@gmail.com>
2024-11-06 17:55:55 +0200

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,34 @@
+import { Github, Linkedin, Mail } from "lucide-react";
+
+export function Footer() {
+  return (
+    <footer className="bg-background/80 backdrop-blur-sm border-t">
+      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col items-center">
+          <div className="flex space-x-6">
+            <a
+              href="https://github.com/Selaelo1"
+              className="text-gray-400 hover:text-purple-400"
+            >
+              <Github className="h-6 w-6" />
+            </a>
+            <a
+              href="www.linkedin.com/in/selaelo-langa"
+              className="text-gray-400 hover:text-purple-400"
+            >
+              <Linkedin className="h-6 w-6" />
+            </a>
+            <a href="#" className="text-gray-400 hover:text-purple-400">
+              <Mail className="h-6 w-6" />
+            </a>
+          </div>
+          <div className="mt-8">
+            <p className="text-center text-base text-gray-400">
+              © {new Date().getFullYear()} Selaelo Langa. All rights reserved.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
