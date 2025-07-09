d0a8d2c602471d92ffda66473cf30f8c4934dccb
Selaelo2 <selaelolanga10@gmail.com>
2025-07-09 17:15:24 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { ThemeProvider } from "./components/ThemeProvider";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
 import WhatsAppButton from "./components/WhatsAppButton";
@@ -13,23 +12,21 @@ import { Toaster } from "./components/ui/sonner";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark">
-      <div className="min-h-screen bg-background text-foreground">
-        <Navbar />
-        <main>
-          <TerminalHero />
-          <About />
-          <TechStackGrid />
-          <ExperienceTimeline />
-          <Services />
-          <ProjectsShowcase />
-          <Contact />
-        </main>
-        <Footer />
-        <WhatsAppButton />
-        <Toaster />
-      </div>
-    </ThemeProvider>
+    <div className="min-h-screen bg-background text-foreground">
+      <Navbar />
+      <main>
+        <TerminalHero />
+        <About />
+        <TechStackGrid />
+        <ExperienceTimeline />
+        <Services />
+        <ProjectsShowcase />
+        <Contact />
+      </main>
+      <Footer />
+      <WhatsAppButton />
+      <Toaster />
+    </div>
   );
 }
 
