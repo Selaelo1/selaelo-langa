6909b539057ae92d1d1e6f941ea2294053a4a45a
Selaelo2 <selaelolanga10@gmail.com>
2025-07-09 17:11:30 +0200

    Update ThemeProvider.tsx

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext, useEffect, useState } from "react";
 
